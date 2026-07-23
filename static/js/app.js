6652c69256c3d4e8bcf35e98881d1859110693ac
EBi <ebi.x2003x@gmail.com>
2026-07-23 13:24:59 +0330

    prettier formatting

--- static/js/app.js
+++ static/js/app.js
@@ -5,19 +5,20 @@
  * If the response is not ok, throws an error with the server message.
  */
 async function apiFetch(url, options = {}) {
-    const headers = options.body && !(options.body instanceof FormData)
-        ? { 'Content-Type': 'application/json', ...options.headers }
-        : { ...options.headers };
+  const headers =
+    options.body && !(options.body instanceof FormData)
+      ? { "Content-Type": "application/json", ...options.headers }
+      : { ...options.headers };
 
-    const response = await fetch(url, {
-        ...options,
-        headers
-    });
+  const response = await fetch(url, {
+    ...options,
+    headers,
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (!response.ok) {
-        throw new Error(data.error || data.message || 'An error occurred');
-    }
-    return data;
+  if (!response.ok) {
+    throw new Error(data.error || data.message || "An error occurred");
+  }
+  return data;
 }
